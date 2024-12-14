fc63eb98174892ddbe2b17227fb40f0aaeb29f3d
David Madison <davidmadison95@yahoo.com>
2024-12-14 00:52:04 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-onload = () => {
-    const c = setTimeout(() => {
-      document.body.classList.remove("not-loaded");
-      clearTimeout(c);
+// Wait for the window to fully load
+window.onload = () => {
+    // Use a timeout to remove the "not-loaded" class after 1 second
+    setTimeout(() => {
+        document.body.classList.remove("not-loaded");
     }, 1000);
-  };
+};
